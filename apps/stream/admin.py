4bb7dc59a10ad209224786d4661ea30122157d45
Volkan Zorlu <volkan.zorlu@gmail.com>
2025-01-09 16:35:47 +0300

    ++

--- apps/stream/admin.py
+++ apps/stream/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import StreamImage, Detection
 
-# Register your models here.
+@admin.register(StreamImage)
+class StreamImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'timestamp', 'processed', 'image_preview')
+    list_filter = ('processed', 'timestamp')
+    search_fields = ('id',)
+    readonly_fields = ('timestamp',)
+    ordering = ('-timestamp',)
+
+    def image_preview(self, obj):
+        return f'<img src="{obj.image.url}" width="100" />' if obj.image else ''
+    image_preview.allow_tags = True
+    image_preview.short_description = 'Preview'
+
+@admin.register(Detection)
+class DetectionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'image', 'class_name', 'x_coord', 'y_coord', 'confidence', 'timestamp')
+    list_filter = ('class_name', 'timestamp')
+    search_fields = ('class_name', 'image__id')
+    readonly_fields = ('timestamp',)
+    ordering = ('-timestamp',)
