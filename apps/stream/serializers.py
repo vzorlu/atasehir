296462af332a487f89b92d4b6ecf8d73ee87dd83
Volkan Zorlu <volkan.zorlu@gmail.com>
2025-01-11 14:31:15 +0300

    ++

--- apps/stream/serializers.py
+++ apps/stream/serializers.py
@@ -17,6 +17,19 @@ class StreamImageSerializer(serializers.ModelSerializer):
         read_only_fields = ['timestamp', 'processed']
 
 class DetectionSerializer(serializers.ModelSerializer):
+    lang = serializers.FloatField(source='image.lang', read_only=True)
+    long = serializers.FloatField(source='image.long', read_only=True)
+
     class Meta:
         model = Detection
-        fields = '__all__'
+        fields = [
+            'id',
+            'image',
+            'class_name',
+            'x_coord',
+            'y_coord',
+            'confidence',
+            'timestamp',
+            'lang',
+            'long'
+        ]
