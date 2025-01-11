d4ba88aa9d31f51b3077d1d7b804939ae45443ed
Volkan Zorlu <volkan.zorlu@gmail.com>
2025-01-11 13:18:37 +0300

    ++

--- apps/stream/serializers.py
+++ apps/stream/serializers.py
@@ -1,12 +1,22 @@
 from rest_framework import serializers
 from .models import StreamImage, Detection
 
-class DetectionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Detection
-        fields = ['id', 'image', 'class_name', 'x_coord', 'y_coord', 'confidence', 'timestamp']
-
 class StreamImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = StreamImage
-        fields = ['id', 'image', 'timestamp', 'processed', 'lang', 'long', 'fulladdress', 'deviceuuid']
+        fields = [
+            'id',
+            'image',
+            'timestamp',
+            'processed',
+            'lang',
+            'long',
+            'fulladdress',
+            'deviceuuid'
+        ]
+        read_only_fields = ['timestamp', 'processed']
+
+class DetectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Detection
+        fields = '__all__'
