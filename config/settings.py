62a80419aecc45080c14246f8fb26d9afc448908
Volkan Zorlu <volkan.zorlu@gmail.com>
2025-01-22 23:45:01 +0300

    ++

--- config/settings.py
+++ config/settings.py
@@ -121,23 +121,14 @@ ASGI_APPLICATION = "config.asgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "atasehir_db",
+        "USER": "atasehir_user",
+        "PASSWORD": "your_password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-"""
 
 AUTH_PASSWORD_VALIDATORS = [
     {
