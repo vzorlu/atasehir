49466285acfe02dbf05bbcc87881e33c569ac5e7
Volkan Zorlu <volkan.zorlu@gmail.com>
2025-01-22 14:51:59 +0300

    ++

--- council/views.py
+++ council/views.py
@@ -5,11 +5,8 @@ from django.conf import settings
 
 
 class CouncilView(TemplateView):
-    template_name = "harita-raporlari.html"
-
     def get_context_data(self, **kwargs):
         context = TemplateLayout.init(self, super().get_context_data(**kwargs))
         context["stream_images"] = StreamImage.objects.prefetch_related("detections").all()
         context["google_maps_api_key"] = settings.GOOGLE_MAPS_API_KEY
-        print("google_maps_api_key", context["google_maps_api_key"])
         return context
