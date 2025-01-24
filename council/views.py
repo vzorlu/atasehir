33d0d6ddda068c3edef0cd7f2521dfe97c4b0b86
Volkan Zorlu <volkan.zorlu@gmail.com>
2025-01-24 21:31:10 +0300

    ++

--- council/views.py
+++ council/views.py
@@ -2,6 +2,7 @@ from django.views.generic import TemplateView
 from stream.models import StreamImage
 from web_project import TemplateLayout
 from django.conf import settings
+from django.shortcuts import render
 
 
 class CouncilView(TemplateView):
@@ -11,41 +12,16 @@ class CouncilView(TemplateView):
         context = TemplateLayout.init(self, super().get_context_data(**kwargs))
 
         # Get images with detections, including all required fields
-        images_with_detections = StreamImage.objects.filter().prefetch_related("detections").order_by("timestamp")
+        images_with_detections = StreamImage.objects.filter().prefetch_related("detections").order_by("-timestamp")
 
         context["stream_images"] = images_with_detections
         context["google_maps_api_key"] = settings.GOOGLE_MAPS_API_KEY
         return context
 
 
-"""
-from django.views.generic import TemplateView
-from django.db.models import Count
-from stream.models import StreamImage
-from web_project import TemplateLayout
-from django.conf import settings
+def images_with_detections(request):
+    stream_images = StreamImage.objects.all().order_by("-timestamp")
 
+    context = {"stream_images": stream_images, "layout_path": "layouts/content.html"}
 
-class CouncilView(TemplateView):
-    template_name = "harita-raporlari.html"
-
-    def get_context_data(self, **kwargs):
-        context = TemplateLayout.init(self, super().get_context_data(**kwargs))
-
-        # Get images with detections, including all required fields
-        images_with_detections = (
-            StreamImage.objects.filter(
-                id__in=StreamImage.objects.values("id")
-                .annotate(detection_count=Count("detections"))
-                .filter(detection_count__gt=0)
-                .values_list("id", flat=True)
-            )
-            .prefetch_related("detections")
-            .order_by("-timestamp")
-        )
-
-        context["stream_images"] = images_with_detections
-        context["google_maps_api_key"] = settings.GOOGLE_MAPS_API_KEY
-        return context
-
-"""
+    return render(request, "raporlar.html", context)
