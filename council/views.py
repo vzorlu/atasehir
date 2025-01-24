63f3600285164fd2bc21d106be34b189f1e32e72
Volkan Zorlu <volkan.zorlu@gmail.com>
2025-01-24 21:34:45 +0300

    ++

--- council/views.py
+++ council/views.py
@@ -12,40 +12,7 @@ class CouncilView(TemplateView):
 
         # Get images with detections, including all required fields
         images_with_detections = StreamImage.objects.all().order_by("-timestamp")
-
+        print("0101010-", images_with_detections)
         context["stream_images"] = images_with_detections
         context["google_maps_api_key"] = settings.GOOGLE_MAPS_API_KEY
         return context
-
-
-"""
-from django.views.generic import TemplateView
-from django.db.models import Count
-from stream.models import StreamImage
-from web_project import TemplateLayout
-from django.conf import settings
-
-
-class CouncilView(TemplateView):
-    template_name = "harita-raporlari.html"
-
-    def get_context_data(self, **kwargs):
-        context = TemplateLayout.init(self, super().get_context_data(**kwargs))
-
-        # Get images with detections, including all required fields
-        images_with_detections = (
-            StreamImage.objects.filter(
-                id__in=StreamImage.objects.values("id")
-                .annotate(detection_count=Count("detections"))
-                .filter(detection_count__gt=0)
-                .values_list("id", flat=True)
-            )
-            .prefetch_related("detections")
-            .order_by("-timestamp")
-        )
-
-        context["stream_images"] = images_with_detections
-        context["google_maps_api_key"] = settings.GOOGLE_MAPS_API_KEY
-        return context
-
-"""
