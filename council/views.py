c60daed0af02c28135899c4bbd4de917a92cf344
Volkan Zorlu <volkan.zorlu@gmail.com>
2025-01-25 14:35:40 +0300

    ++

--- council/views.py
+++ council/views.py
@@ -20,10 +20,9 @@ class CouncilView(TemplateView):
 
 
 def images_with_detections(request):
-    stream_images = StreamImage.objects.prefetch_related("detection_set").order_by(
-        "-id"
-    )  # Fetch related detections efficiently
+    stream_images = StreamImage.objects.prefetch_related(
+        "detections"
+    ).order_by("-id")  # Note: changed from detection_set to detections
 
     context = {"stream_images": stream_images, "layout_path": "layouts/content.html"}
-
     return render(request, "raporlar.html", context)
