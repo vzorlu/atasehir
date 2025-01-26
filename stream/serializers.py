da97ebd6c1e28b3b3b8bdf3cd157fbe0eea0a1d1
Volkan Zorlu <volkan.zorlu@gmail.com>
2025-01-26 14:47:00 +0300

    ++

--- stream/serializers.py
+++ stream/serializers.py
@@ -5,12 +5,12 @@ from .models import StreamImage, Detection
 class DetectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Detection
-        fields = ["id", "class_name", "x_coord", "y_coord", "confidence", "timestamp"]
+        fields = ["id", "image", "class_name", "x_min", "y_min", "x_max", "y_max", "confidence", "timestamp"]
 
 
 class StreamImageSerializer(serializers.ModelSerializer):
     detections = DetectionSerializer(many=True, read_only=True)
-    detection_count = serializers.IntegerField(read_only=True)
+    detection_count = serializers.IntegerField(read_only=True, required=False)
 
     class Meta:
         model = StreamImage
