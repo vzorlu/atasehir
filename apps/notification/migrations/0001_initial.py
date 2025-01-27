9fe7d9ce0b23b548094a442c2d438d1d3cb190e7
Volkan Zorlu <volkan.zorlu@gmail.com>
2025-01-27 17:35:27 +0300

    ++

--- apps/notification/migrations/0001_initial.py
+++ apps/notification/migrations/0001_initial.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
     initial = True
 
-    dependencies = []
+    dependencies = []  # Remove notification dependency
 
     operations = [
         migrations.CreateModel(
